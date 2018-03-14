a28203c4855ed23052fa08727bafb10e55ff166b
kondraaj <kondraaj@rose-hulman.edu>
2018-03-14 19:46:58 -0400

    all done

--- src/m3.py
+++ src/m3.py
@@ -100,7 +100,7 @@ def run_test_broken_1():
 
 
 # ----------------------------------------------------------------------
-# TODO: 3. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 3. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # ----------------------------------------------------------------------
 def broken_1(m):
@@ -122,8 +122,8 @@ def broken_1(m):
     #    **  For full credit you must appropriately
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     count = 0
-    for k in range(m, 2 * m + 1):
-        if is_prime(k) == True:
+    for k in range(m, (2 * m) + 2):
+        if is_prime(k) is True:
             count = count + 1
 
     return count
